1436c048d433adf77c7293905c47ea2670f46668
Bryan <baijiangjie@gmail.com>
2024-12-04 18:25:47 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -17,6 +17,7 @@ if [[ $changed_files =~ "readmes/README" ]]; then
         echo "Commit..."
         git add $(git diff --name-only)
         git commit -m "Auto-translate README"
-        git push origin dev:pr@dev@translate_readme
+        git switch pr@dev@translate_readmex
+        git push origin -f -v
     fi
 fi
