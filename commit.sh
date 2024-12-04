f09ad7d00161d59ed41c6fc00b520c5cc29970b8
Bryan <baijiangjie@gmail.com>
2024-12-04 15:57:57 +0800

    Create commit.sh

---
+++ commit.sh
@@ -0,0 +1,20 @@
+LATEST_COMMIT_MSG=$(git show -s --format='%s')
+
+git add -N .
+
+dir_files=$(ls)
+changed_files=$(git diff --name-only)
+current_branch=$(git branch --show-current)
+
+echo "Current branch $current_branch"
+echo "The current files: $dir_files"
+echo "You auto changed $changed_files"
+
+if [[ $changed_files =~ "readmes/README" ]]; then
+    if [[ "$LATEST_COMMIT_MSG" != "Auto-translate README" ]]; then
+        echo "Commit..."
+        git add $(git diff --name-only)
+        git commit -m "Auto-translate README"
+        git push origin dev:pr@dev@translate_readme
+    fi
+fi
