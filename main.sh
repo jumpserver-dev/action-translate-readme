370488f259498d8aa8f900659af4574174e26829
Bryan <baijiangjie@gmail.com>
2024-12-05 10:47:00 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,21 +4,24 @@
 # OPENAI_API_KEY
 # GPT_MODE
 # TARGET_LANGUAGES
+# PUSH_BRANCH
+
+
+PUSH_BRANCH=${PUSH_BRANCH:-pr@dev@translate_readme}
 
 function add_remote_github() {
   # Clone仓库
-  git config --global user.name "BaiJiangJie"
-  git config --global user.email "jiangjie.bai@fit2cloud.com"
+  git config --global user.name "github-actions[bot]"
+  git config --global user.email "github-actions[bot]@users.noreply.github.com"
   remote_url="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
   rm -rf GITHUB_REPO
   git clone "${remote_url}" "GITHUB_REPO" && cd "GITHUB_REPO" || exit 2
-  cp /translation.py ./
   git status
   git fetch origin
   git remote -v
 }
 
-commit_push_github() {
+function commit_push_github() {
   LATEST_COMMIT_MSG=$(git show -s --format='%s')
   
   git add -N .
@@ -27,23 +30,23 @@ commit_push_github() {
   changed_files=$(git diff --name-only | grep "readmes/")
   current_branch=$(git branch --show-current)
   
-  echo "Current branch $current_branch"
   echo "You auto changed $changed_files"
   
   if [[ $changed_files =~ "readmes/README" ]]; then
       if [[ "$LATEST_COMMIT_MSG" != "Auto-translate README" ]]; then
           echo "Commit..."
-          git add $(git diff --name-only)
+          git add "${changed_files}"
           git commit -m "Auto-translate README"
-          git switch -c pr@dev@translate_readmex
-          git push origin -f -v pr@dev@translate_readmex 
+          git switch -c ${PUSH_BRANCH}
+          git push origin -f -v ${PUSH_BRANCH} 
       fi
   fi
 }
 
-translate() {
+function translate() {
   # 添加 github 认证
   add_remote_github
+  cp /translation.py ./
   python translation.py
   commit_push_github
 }
